4ad34ea83eb6a4126dc34cb5d9e3b1120c742373
Jonas Ha <61319894+j-d-ha@users.noreply.github.com>
2025-11-03 19:34:29 -0500

    build: update IntelliJ Platform and Gradle versions (#33)
    - Upgraded IntelliJ Platform to 2.10.4.
    - Updated Gradle to version 9.2.0.
    - Improved plugin verification to specify release channels and added detailed logging.
    - Simplified loading of `.env` file in the build script.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,14 +3,10 @@ import java.util.Properties
 import org.jetbrains.changelog.Changelog
 import org.jetbrains.changelog.markdownToHTML
 import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+import org.jetbrains.intellij.platform.gradle.models.ProductRelease.Channel
 
 // load .env file if it exists
-File(".env").takeIf(File::exists)?.let {
-    Properties().apply {
-        load(FileReader(it))
-        println(".env file found")
-    }
-}
+File(".env").takeIf(File::exists)?.let { Properties().apply { load(FileReader(it)) } }
 
 plugins {
     id("java") // Java support
@@ -129,12 +125,19 @@ intellijPlatform {
 
     pluginVerification {
         ides {
-            val productReleases = ProductReleasesValueSource().get()
+            val productReleases =
+                ProductReleasesValueSource { this.channels = listOf(Channel.RELEASE) }.get()
+
             val reducedProductReleases =
                 if (productReleases.size > 2)
                     listOf(productReleases.first(), productReleases.last())
                 else productReleases
-            ides(reducedProductReleases)
+
+            reducedProductReleases.forEach { notation ->
+                val (type, version) = notation.split("-", limit = 2)
+                logger.info("Verifying plugin against $type $version")
+                create(type, version) { useInstaller = false }
+            }
         }
     }
 }
